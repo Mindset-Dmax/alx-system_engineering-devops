9cd11d3d61b6995bed3f55802da035864b7e1bfc
Mindset-Dmax <emmanuelkipchirchir16@gmail.com>
2024-04-27 17:18:14 +0300

    Creating a temporary file in puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Creating tmp file in puppet configuration
+file { '/tmp/school':
+ensure  => present,
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
+content => 'I love Puppet'
+}
